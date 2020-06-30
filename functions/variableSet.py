f4be95f9a5276a37233c0171f9c6982525f5875c
almadaocta <almadaocta@gmail.com>
2020-06-30 15:00:34 -0300

    final

--- functions/variableSet.py
+++ functions/variableSet.py
@@ -16,13 +16,12 @@ def set_variables(calcData):
     s = lx*ly # Superficie
     B = (E/(1-o**2))*((t**3)/12) # Rigidez a la flexión 
     Fc = (c**2/(2*math.pi))*(sqrt(sd/B)) #Frecuencia crítica
-
+    Fc= round(Fc,1)
     Fd = (E/(2*math.pi*p))*(sqrt(sd/B)) # Frecuencia de densidad
     f11 = (c**2/(4*Fc))*((1/lx**2)+(1/ly**2)) #Modo (1,1) de placa del elemento
 
 
     #Asigno variables al diccionario
-
     calcData['c'] = c #Velocidad del sonido en el aire
     calcData['p0'] = p0 #Densidad del aire
     calcData['sd'] = sd #Densidad superficial
@@ -33,4 +32,4 @@ def set_variables(calcData):
     calcData['f11'] = f11 #Modo (1,1) de placa del elemento
 
     
-
+    print(Fc)
